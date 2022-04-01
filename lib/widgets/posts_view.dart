4e741d9c80b7064fde3cc7bafad80070efc88726
Kim Huyen <huyenkim0208@gmail.com>
2022-04-01 12:09:22 +0700

    update count like & comment

--- lib/widgets/posts_view.dart
+++ lib/widgets/posts_view.dart
@@ -132,5 +132,26 @@ class _PostsState extends State<Posts> {
     );
   }
 
-  
+  buildLikesCount(BuildContext context, int count) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 7.0),
+      child: Text(
+        '$count likes',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 10.0,
+        ),
+      ),
+    );
+  }
+
+  buildCommentsCount(BuildContext context, int count) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 0.5),
+      child: Text(
+        '-   $count comments',
+        style: TextStyle(fontSize: 8.5, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
 
